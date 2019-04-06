2939b3d2aaf440d5386a1f264e4c67eba6819eab
Gnimuc <qiyupei@gmail.com>
2019-04-06 11:13:09 +0800

    Add an example for testing custom callbacks

---
+++ examples/custom_callback.jl
@@ -0,0 +1,87 @@
+using CImGui
+using CImGui.CSyntax
+using CImGui.CSyntax.CStatic
+using CImGui.GLFWBackend
+using CImGui.OpenGLBackend
+using CImGui.GLFWBackend.GLFW
+using CImGui.OpenGLBackend.ModernGL
+using Printf
+
+@static if Sys.isapple()
+    # OpenGL 3.2 + GLSL 150
+    const glsl_version = 150
+    GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 3)
+    GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 2)
+    GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE) # 3.2+ only
+    GLFW.WindowHint(GLFW.OPENGL_FORWARD_COMPAT, GL_TRUE) # required on Mac
+else
+    # OpenGL 3.0 + GLSL 130
+    const glsl_version = 130
+    GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 3)
+    GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 0)
+    # GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE) # 3.2+ only
+    # GLFW.WindowHint(GLFW.OPENGL_FORWARD_COMPAT, GL_TRUE) # 3.0+ only
+end
+
+# setup GLFW error callback
+error_callback(err::GLFW.GLFWError) = @error "GLFW ERROR: code $(err.code) msg: $(err.description)"
+GLFW.SetErrorCallback(error_callback)
+
+# custom callbacks
+function CustomUserCallbackMousebutton(window::GLFW.Window, button::GLFW.MouseButton, action::GLFW.Action, mods::Cint)
+    println("Trigger user mouse button callback!")
+    return nothing
+end
+SetCustomMouseButtonCallback(CustomUserCallbackMousebutton)
+
+# create window
+window = GLFW.CreateWindow(1280, 720, "Demo")
+@assert window != C_NULL
+GLFW.MakeContextCurrent(window)
+GLFW.SwapInterval(1)  # enable vsync
+
+# setup Dear ImGui context
+ctx = CImGui.CreateContext()
+
+# setup Dear ImGui style
+CImGui.StyleColorsDark()
+
+# setup Platform/Renderer bindings
+ImGui_ImplGlfw_InitForOpenGL(window, true)
+ImGui_ImplOpenGL3_Init(glsl_version)
+
+show_demo_window = true
+clear_color = Cfloat[0.45, 0.55, 0.60, 1.00]
+while !GLFW.WindowShouldClose(window)
+    # oh my global scope
+    global show_demo_window
+    global clear_color
+
+    GLFW.PollEvents()
+    # start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame()
+    ImGui_ImplGlfw_NewFrame()
+    CImGui.NewFrame()
+
+    # show the big demo window
+    show_demo_window && @c CImGui.ShowDemoWindow(&show_demo_window)
+
+    # rendering
+    CImGui.Render()
+    GLFW.MakeContextCurrent(window)
+    display_w, display_h = GLFW.GetFramebufferSize(window)
+    glViewport(0, 0, display_w, display_h)
+    glClearColor(clear_color...)
+    glClear(GL_COLOR_BUFFER_BIT)
+    ImGui_ImplOpenGL3_RenderDrawData(CImGui.GetDrawData())
+
+    GLFW.MakeContextCurrent(window)
+    GLFW.SwapBuffers(window)
+end
+
+# cleanup
+ImGui_ImplOpenGL3_Shutdown()
+ImGui_ImplGlfw_Shutdown()
+CImGui.DestroyContext(ctx)
+
+GLFW.DestroyWindow(window)

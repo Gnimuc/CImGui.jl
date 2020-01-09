b8c713726ad5b812356e26588169fe8c74054369
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2020-01-09 02:35:12 +0100

    use Refs instead of introducing new global variables in __init__ (#16)
    * stop changing bindings of global variables

--- src/backend/GLFW/callbacks.jl
+++ src/backend/GLFW/callbacks.jl
@@ -1,13 +1,11 @@
 # custom callbacks
-SetCustomMouseButtonCallback(x::Function) = global g_CustomCallbackMousebutton = x
-SetCustomScrollCallback(x::Function) = global g_CustomCallbackScroll = x
-SetCustomKeyCallback(x::Function) = global g_CustomCallbackKey = x
-SetCustomCharCallback(x::Function) = global g_CustomCallbackChar = x
+SetCustomMouseButtonCallback(x::Function) = g_CustomCallbackMousebutton[] = x
+SetCustomScrollCallback(x::Function) = g_CustomCallbackScroll[] = x
+SetCustomKeyCallback(x::Function) = g_CustomCallbackKey[] = x
+SetCustomCharCallback(x::Function) = g_CustomCallbackChar[] = x
 
 function ImGui_ImplGlfw_MouseButtonCallback(window::GLFW.Window, button::GLFW.MouseButton, action::GLFW.Action, mods::Cint)
-    global g_MouseJustPressed
-    global g_CustomCallbackMousebutton
-    g_CustomCallbackMousebutton != C_NULL && g_CustomCallbackMousebutton(window, button, action, mods)
+    g_CustomCallbackMousebutton[] != C_NULL && g_CustomCallbackMousebutton[](window, button, action, mods)
     b = Cint(button)
     if action == GLFW.PRESS && b ≥ 0 && b < length(g_MouseJustPressed)
         g_MouseJustPressed[b+1] = true
@@ -15,16 +13,14 @@ function ImGui_ImplGlfw_MouseButtonCallback(window::GLFW.Window, button::GLFW.Mo
 end
 
 function ImGui_ImplGlfw_ScrollCallback(window::GLFW.Window, xoffset, yoffset)
-    global g_CustomCallbackScroll
-    g_CustomCallbackScroll != C_NULL && g_CustomCallbackScroll(window, xoffset, yoffset)
+    g_CustomCallbackScroll[] != C_NULL && g_CustomCallbackScroll[](window, xoffset, yoffset)
     io = GetIO()
     io.MouseWheelH += Cfloat(xoffset)
     io.MouseWheel += Cfloat(yoffset)
 end
 
 function ImGui_ImplGlfw_KeyCallback(window::GLFW.Window, key, scancode, action, mods)
-    global g_CustomCallbackKey
-    g_CustomCallbackKey != C_NULL && g_CustomCallbackKey(window, key, scancode, action, mods)
+    g_CustomCallbackKey[] != C_NULL && g_CustomCallbackKey[](window, key, scancode, action, mods)
 
     io = GetIO()
     action == GLFW.PRESS && Set_KeysDown(io, key, true)
@@ -37,8 +33,7 @@ function ImGui_ImplGlfw_KeyCallback(window::GLFW.Window, key, scancode, action, 
 end
 
 function ImGui_ImplGlfw_CharCallback(window::GLFW.Window, c)
-    global g_CustomCallbackChar
-    g_CustomCallbackChar != C_NULL && g_CustomCallbackChar(window, c)
+    g_CustomCallbackChar[] != C_NULL && g_CustomCallbackChar[](window, c)
     io = GetIO()
     0 < Cuint(c) < 0x10000 && AddInputCharacter(io, c)
 end

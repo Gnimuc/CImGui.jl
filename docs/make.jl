ed0a44447eb9d9bef54279f7b3bda0a6fe72b947
Gnimuc <qiyupei@gmail.com>
2020-05-23 20:52:06 +0800

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,22 @@
-using Documenter, CImGui
+using CImGui
+using Documenter
 
-makedocs(
-    modules = [CImGui],
-    clean = false,
-    sitename = "CImGui.jl",
-    linkcheck = !("skiplinks" in ARGS),
-    pages = [
-        "Introduction" => "index.md",
-        "API Reference" => "api.md",
+makedocs(;
+    modules=[CImGui],
+    authors="Yupei Qi <qiyupei@gmail.com>",
+    repo="https://github.com/Gnimuc/CImGui.jl/blob/{commit}{path}#L{line}",
+    sitename="CImGui.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://Gnimuc.github.io/CImGui.jl",
+        assets=String[],
+    ),
+    pages=[
+       "Introduction" => "index.md",
+       "API Reference" => "api.md",
     ],
 )
 
-deploydocs(
-    repo = "github.com/Gnimuc/CImGui.jl.git",
+deploydocs(;
+    repo="github.com/Gnimuc/CImGui.jl.git",
 )
